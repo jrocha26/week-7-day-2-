aa7b58b2e0615b4258f37d393d20d46be912ee5e
Janette Rocha <113930409+jrocha26@users.noreply.github.com>
2022-10-06 17:54:15 +0000

    use zip to combine tuple in a list

--- zip.py
+++ zip.py
@@ -1,4 +1,4 @@
-def zip():
+def zip1():
   # zip
   # Notice the format enumerate actually returns, let's take a look by transforming it to a list()
   # list(enumerate('abcde'))
@@ -12,7 +12,7 @@ def zip():
   mylist2 = ['a','b','c','d','e']
   
   # This one is also a generator! We will explain this later, but for now let's transform it to a list
-  zip(mylist1,mylist2)
+  print(list(zip(mylist1, mylist2)))
   
   
   # list(zip(mylist1,mylist2))
@@ -52,16 +52,16 @@ def zip():
   
   # Use the zip function, loops, and the following lists of countries and capitals to solve it quickly and efficiently.
   
-  # capitals = ["Berlin", "Tokyo", "Paris", "Helsinki", "Ottawa", "Canberra"]
-  # countries = ["Germany", "Japan", "France", "Finland", "Canada", "Australia"]
-  
+  capitals = ["Berlin", "Tokyo", "Paris", "Helsinki", "Ottawa", "Canberra"]
+  countries = ["Germany", "Japan", "France", "Finland", "Canada", "Australia"]
+  print(list(zip(capitals, countries)))
   
   
   # Zip Practice #2
   # Create a zip object made up of lists, of a set of brands and products that you prefer, inside the my_zip variable.
-  # brands =
-  # products =
-  
+  brands = ["elf", "maybeline", "estee lauder", "milani"]
+  products = ["powder", "mascara", "lipstick", "lipliner"]
+  print(list(zip(brands, products)))
   
   # Zip Practice #3
   # Create a zip object with the translations of the numbers from 1 to 5 in Spanish, Portuguese and English (in that same order), and then convert the generated object into a list called numbers:
